232bfd4d92fe9ca4b4ea14e0648606b163806f42
quArex-rQer <77916984+quArex-rQer@users.noreply.github.com>
2022-06-08 17:37:54 +0300

    Add files via upload
    Henüz bitmemiştir fakat yakın zamanda bitirmeyi planlıyorum.

---
+++ routes/hotels.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getAllHotels,
+  getHotel,
+  updateHotel,
+} from '../controllers/hotel.js';
+import Hotel from '../models/Hotel.js';
+import {verifyAdmin} from '../utils/verifyToken.js';
+
+const router = express.Router();
+
+// CREATE
+router.post('/', verifyAdmin, createHotel);
+// UPDATE
+router.put('/:id', verifyAdmin, updateHotel);
+// DELETE
+router.delete('/:id', verifyAdmin, deleteHotel);
+// GET
+router.get('/find/:id', getHotel);
+// GET ALL
+router.get('/', getAllHotels);
+router.get('/countByCity', countByCity);
+router.get('/countByType', countByType);
+
+export default router;
